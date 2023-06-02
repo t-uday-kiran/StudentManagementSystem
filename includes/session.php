320a125e800da9f83264b92e3a33337f56055e79
F tech <121655842+f-tech-solutions@users.noreply.github.com>
2023-06-02 11:37:05 +0530

    Added Landing Page

--- includes/session.php
+++ includes/session.php
@@ -3,7 +3,7 @@
 //Check whether the session variable SESS_MEMBER_ID is present or not
 if (!isset($_SESSION['alogin']) || (trim($_SESSION['alogin']) == '')) { ?>
 <script>
-window.location = "../index.php";
+window.location = "../index.html";
 </script>
 <?php
 }
